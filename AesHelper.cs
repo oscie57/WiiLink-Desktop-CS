9323434d8ea45dc2a74381bb8d6b4fa630fd24a8
oscie <hudso009@gmail.com>
2024-06-28 09:25:51 +0100

    aeshelper (ty copilot)

---
+++ AesHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiiLink_Desktop_CS
+{
+    public class AesHelper
+    {
+        private byte[] key;
+        private byte[] iv;
+
+        public AesHelper(byte[] key, byte[] iv)
+        {
+            this.key = key;
+            this.iv = iv;
+        }
+
+        public string Decrypt(byte[] encryptedData)
+        {
+            using (AesManaged aesAlg = new AesManaged())
+            {
+                aesAlg.Key = key;
+                aesAlg.IV = iv;
+                aesAlg.Mode = CipherMode.CBC;
+                aesAlg.Padding = PaddingMode.PKCS7;
+
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msDecrypt = new MemoryStream(encryptedData))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            return srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
