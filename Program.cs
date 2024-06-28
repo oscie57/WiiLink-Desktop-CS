47399da31f54d351ff9c58d11b8b3694cc15bb51
oscie <hudso009@gmail.com>
2024-06-28 09:35:48 +0100

    serialisation and decryption

--- Program.cs
+++ Program.cs
@@ -7,6 +7,10 @@ using System.Text.Json;
 using System.IO;
 using System.Text.Json.Serialization;
 using System.Media;
+using System.Net;
+using System.Drawing.Text;
+using System.Net.Http;
+using System.Xml.Serialization;
 
 namespace WiiLink_Desktop_CS
 {
@@ -20,9 +24,6 @@ namespace WiiLink_Desktop_CS
         public static SoundPlayer BGM_Settings = new(Properties.Resources.BGM_Settings);
         public static SoundPlayer BGM_Theatre = new(Properties.Resources.BGM_Theatre);
 
-        public static string FirstBinURL;
-        public static bool maintenance;
-
         public static void LoadAppConfig()
         {    
             if (!File.Exists("config.json"))
@@ -35,12 +36,31 @@ namespace WiiLink_Desktop_CS
             Config = JsonSerializer.Deserialize<RootConfig>(configtext);
         }
 
-        public static void LoadFirstConfig()
+        public static async void LoadFirstConfig()
         {
-            FirstBinURL = $"{Config.ServerURL}/conf/first.bin";
-            maintenance = false;
+            byte[] FIRST_BIN_KEY = new byte[] { 0x94, 0x3B, 0x13, 0xDD, 0x87, 0x46, 0x8B, 0xA5, 0xD9, 0xB7, 0xA8, 0xB8, 0x99, 0xF9, 0x18, 0x03 };
+            byte[] FIRST_BIN_IV = new byte[] { 0x66, 0xB3, 0x3F, 0xC1, 0x37, 0x3F, 0xE5, 0x06, 0xEC, 0x2B, 0x59, 0xFB, 0x6B, 0x97, 0x7C, 0x82 };
 
-            FirstConfig.Maintenance = false;
+            string FirstBinURL = $"{Config.ServerURL}/conf/first.bin";
+
+            HttpClient SharedClient = new();
+            SharedClient.BaseAddress = new Uri(FirstBinURL);
+            SharedClient.DefaultRequestHeaders.UserAgent.Add(new("Gex", "2"));
+
+            using HttpResponseMessage response = await SharedClient.GetAsync(SharedClient.BaseAddress);
+            response.EnsureSuccessStatusCode();
+            
+            byte[] EncryptedXML = await response.Content.ReadAsByteArrayAsync();
+
+            AesHelper AesHelper = new(FIRST_BIN_KEY, FIRST_BIN_IV);
+            string DecryptedXML = AesHelper.Decrypt(EncryptedXML);
+
+            XmlSerializer Serializer = new(typeof(FirstConfig));
+
+            using (TextReader Reader = new StringReader(DecryptedXML))
+            {
+                FirstConfig = (FirstConfig)Serializer.Deserialize(Reader);
+            }
         }
 
         /// <summary>
@@ -54,7 +74,8 @@ namespace WiiLink_Desktop_CS
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            if (maintenance == false)
+
+            if ( !FirstConfig.maint )
             {
                 Application.Run(new Form_Main());
             }
